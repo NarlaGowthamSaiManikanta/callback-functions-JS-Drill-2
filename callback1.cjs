25889bb6ce3a8d4e657a1ed46bcdf2819c510a52
Narla Gowtham Sai Manikanta <narla.manikanta.27.1@mountblue.tech>
2024-02-10 18:09:17 +0530

    Refactored callback1.cjs and test/testCallback1.js for promises.

--- callback1.cjs
+++ callback1.cjs
@@ -1,37 +1,29 @@
 const fs = require('fs');
 
-function readFileData(directory, filename, encoding, cb) {
-    setTimeout(() =>{
-        let filepath = `${directory}/${filename}`;
-        fs.readFile(filepath, encoding, (err, data) => {
-            if(err) {
-                throw err;
-            } else {
-                if(typeof cb === 'function'){
-                    cb(JSON.parse(data));
-                } else{
-                    console.log(data);
-                }
-            }
-        });
-    }, 2000);
-}
+function findingBoardInformation(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                let filePath = `${data.directory}/${data.filename}`;
 
-function printBoardInformation(boardsData, boardID) {
-    setTimeout(() => {
-        for (let board of boardsData) {
-            if(board.id === boardID) {
-                for (let info in board) {
-                    if (!board[info] || Object.keys(board[info]).length === 0) {
-                        board[info] = 'NA';
+                fs.readFile(filePath, data.encoding, (err, boards) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        for (let board of JSON.parse(boards)) {
+                            if (board[data.property] === data.value) {
+                                resolve(board);
+                            }
+                        }
+
+                        reject('Board is not Available');
                     }
-                    console.log(`${info}: ${board[info]}`);
-                }
+                });
+            } catch (error) {
+                reject(error);
             }
-        }
-    }, 2000)
+        }, 2 * 1000);
+    });
 }
 
-module.exports = {
-    readFileData, printBoardInformation
-}
+module.exports = findingBoardInformation;
--- test/testCallback1.js
+++ test/testCallback1.js
@@ -1,13 +1,15 @@
-const {
-    readFileData, printBoardInformation
-} = require('../callback1.cjs');
+const findingBoardInformation = require('../callback1.cjs');
 
 const directory = 'data';
 const filename = 'boards_1.json';
 const encoding = 'utf-8';
-const boardID = 'mcu453ed';
+const property = 'id';
+const value = 'mcu453ed';
 
-readFileData(directory, filename, encoding, (boardsData) => {
-    console.log("Board's Info :- ");
-    printBoardInformation(boardsData, boardID);
-});
+findingBoardInformation({directory, filename, encoding, property, value})
+    .then((board) => {
+        console.log(board);
+    })
+    .catch((rejectedReason) => {
+        console.log(rejectedReason);
+    });
